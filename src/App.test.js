7aae0c0cde7e604e23bfd7129d6a98c34611d7b1
vn509qm <omar.naeem@walmart.com>
2020-05-09 21:58:54 +0500

    App component updated

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { act } from 'react-dom/test-utils';
 import App from './App';
-import { ListItem } from './components';
+import { ListItem, Input, Button } from './components';
 import { Todos } from './pages';
 
 jest.mock('./apis/todos.js');
@@ -17,11 +17,30 @@ it('renders without crashing', () => {
   expect(wrapper).toMatchSnapshot();
 });
 
-it('mount whole component with mocks', () => {
-  const wrapper = mount(<App />);
+it('mount whole component with mocks', async () => {
+  let wrapper;
+  await act(() => {
+    wrapper = mount(<App />);
+  });
+  await wrapper.update();
   expect(wrapper).toMatchSnapshot();
+  expect(wrapper.find(ListItem)).toHaveLength(2);
+
   // adding an item
-  wrapper.find('input').simulate('change', { target: { value: 'item3' } });
-  wrapper.find('button').simulate('click');
+  await act(() => {
+    wrapper.find(Input).find('input').simulate('change', { target: { value: 'item3' } });
+    wrapper.find(Button).find('button').simulate('click');
+  });
+  await wrapper.update();
   expect(wrapper).toMatchSnapshot();
+  expect(wrapper.find(ListItem)).toHaveLength(3);
+  console.log(wrapper.find(ListItem).map(each => each.text()))
+
+  // removing last item
+  await act(() => {
+    wrapper.find('[className="cancelButton"]').at(0).simulate('click');
+  });
+  await wrapper.update();
+  expect(wrapper).toMatchSnapshot();
+  expect(wrapper.find(ListItem)).toHaveLength(2);
 });
