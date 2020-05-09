c9e5d3b10ffbf213dc117632300e00e33fabc65c
vn509qm <omar.naeem@walmart.com>
2020-05-09 22:03:56 +0500

    achieved 100% coverage

--- src/apis/todos.spec.js
+++ src/apis/todos.spec.js
@@ -2,37 +2,75 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { getList, addItem, removeItem } from '.';
 
-beforeEach(() => {
-  localStorage.clear();
-  localStorage.setItem('list', "[\"item1\", \"item2\"]")
-});
-
-it('getList', async () => {
-  const data = await getList();
-  expect(data).toStrictEqual({
-    status: 200,
-    message: 'success',
-    data: ['item1', 'item2']
+describe('with existing data in localStorage', () => {
+  beforeEach(() => {
+    localStorage.clear();
+    localStorage.setItem('list', "[\"item1\", \"item2\"]")
   });
-});
 
-it('addItem', () => {
-  addItem('item3').then((data) => {
+  it('getList', async () => {
+    const data = await getList();
     expect(data).toStrictEqual({
       status: 200,
       message: 'success',
-      data: ['item1', 'item2', 'item3']
+      data: ['item1', 'item2']
     });
   });
-});
 
-it('removeItem', (done) => {
-  removeItem(1).then((data) => {
+  it('addItem', () => {
+    addItem('item3').then((data) => {
+      expect(data).toStrictEqual({
+        status: 200,
+        message: 'success',
+        data: ['item1', 'item2', 'item3']
+      });
+    });
+  });
+
+  it('removeItem', (done) => {
+    removeItem(1).then((data) => {
+      expect(data).toStrictEqual({
+        status: 200,
+        message: 'success',
+        data: ['item1']
+      });
+      done();
+    }).catch(done.fail);
+  });
+})
+
+describe('with no data in localStorage', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  it('getList', async () => {
+    const data = await getList();
     expect(data).toStrictEqual({
       status: 200,
       message: 'success',
-      data: ['item1']
+      data: []
     });
-    done();
-  }).catch(done.fail);
-});
+  });
+
+  it('addItem', () => {
+    addItem('item3').then((data) => {
+      expect(data).toStrictEqual({
+        status: 200,
+        message: 'success',
+        data: ['item3']
+      });
+    });
+  });
+
+  it('removeItem', (done) => {
+    removeItem(1).then((data) => {
+      expect(data).toStrictEqual({
+        status: 200,
+        message: 'success',
+        data: []
+      });
+      done();
+    }).catch(done.fail);
+  });
+})
