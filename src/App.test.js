034474050dd931ea8c9fd851891afcadb79ccd73
vn509qm <omar.naeem@walmart.com>
2020-05-09 18:38:13 +0500

    App test added with existing localstorage

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
-
+beforeEach(() => {
+  localStorage.clear();
+  localStorage.setItem('list', "[\"item1\", \"item2\"]")
+});
 it('renders without crashing', () => {
-  shallow(<App />);
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
 });
